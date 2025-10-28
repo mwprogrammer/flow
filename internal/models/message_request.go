0e66634fa9734186849e3de63abef0a3cda2fb97
Chisomo Chiweza <chisomochiweza@proton.me>
2025-10-28 19:40:56 +0200

    Add models: - Add generic http_response model for http service - Add cloud api request model - Add message model

---
+++ internal/models/message_request.go
@@ -0,0 +1,95 @@
+package models
+
+type MessageRequest struct {
+	Product            string              `json:"messaging_product"`
+	MessageId          string              `json:"message_id"`
+	Status             string              `json:"status"`
+	ReceipientType     string              `json:"receipient_type"`
+	To                 string              `json:"to"`
+	Type               string              `json:"type"`
+	PreviewUrl         bool                `json:"preview_url"`
+	TextMessage        *TextMessage        `json:"text"`
+	InteractiveMessage *InteractiveMessage `json:"interactive"`
+	ImageMessage       *ImageMessage       `json:"image"`
+	VideoMessage       *VideoMessage       `json:"video"`
+	AudioMessage       *AudioMessage       `json:"audio"`
+	DocumentMessage    *DocumentMessage    `json:"document"`
+	LocationMessage    *LocationMessage    `json:"location"`
+}
+
+type TextMessage struct {
+	Body string `json:"body"`
+}
+
+type InteractiveMessage struct {
+	Type   string `json:"type"`
+	Header Header `json:"header"`
+	Body   Body   `json:"body"`
+	Footer Footer `json:"footer"`
+	Action Action `json:"action"`
+}
+
+type Header struct {
+	Text string `json:"text"`
+}
+
+type Body struct {
+	Text string `json:"text"`
+}
+
+type Footer struct {
+	Text string `json:"text"`
+}
+
+type Action struct {
+	Button   string           `json:"button"`
+	Buttons  []ButtonMessage  `json:"buttons"`
+	Sections []SectionMessage `json:"sections"`
+}
+
+type ButtonMessage struct {
+	Type  string `json:"type"`
+	Reply Button `json:"reply"`
+}
+
+type SectionMessage struct {
+	Title string       `json:"title"`
+	Rows  []SectionRow `json:"rows"`
+}
+
+type SectionRow struct {
+	Id          string `json:"id"`
+	Title       string `json:"title"`
+	Description string `json:"description"`
+}
+
+type ImageMessage struct {
+	Id      int    `json:"id"`
+	Caption string `json:"caption"`
+	Link    string `json:"link"`
+}
+
+type VideoMessage struct {
+	Id      int    `json:"id"`
+	Caption string `json:"caption"`
+	Link    string `json:"link"`
+}
+
+type AudioMessage struct {
+	Id      int    `json:"id"`
+	Caption string `json:"caption"`
+	Link    string `json:"link"`
+}
+
+type DocumentMessage struct {
+	Id   int    `json:"id"`
+	Name string `json:"filename"`
+	Link string `json:"link"`
+}
+
+type LocationMessage struct {
+	Latitude  string `json:"latitude"`
+	Longitude string `json:"longitude"`
+	Name      string `json:"name"`
+	Address   string `json:"address"`
+}
