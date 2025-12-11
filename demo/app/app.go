3989f282920d1ab52f1dba43d0a08a453e9b8289
Chisomo Chiweza <chisomochiweza@proton.me>
2025-12-11 17:45:55 +0200

    Setup demo app to test end to end flow functionality: - Use gin to define endpoints meta will use when pushing requests from it's webhooks - Successfully receive messages from meta

--- demo/app/app.go
+++ demo/app/app.go
@@ -1,13 +1,13 @@
 package demo
 
-import "os"
+// import "os"
 
-func Setup() {
+// func Setup() {
 
-	api_version := os.Getenv("META_WHATSAPP_CLOUD_API_VERSION=")
-	access_token := os.Getenv("META_WHATSAPP_ACCESS_TOKEN")
-	account_id := os.Getenv("META_WHATSAPP_BUSINESS_ID")
-	sender_id := os.Getenv("META_WHATSAPP_SENDER_PHONE_NUMBER")
+// 	api_version := os.Getenv("META_WHATSAPP_CLOUD_API_VERSION=")
+// 	access_token := os.Getenv("META_WHATSAPP_ACCESS_TOKEN")
+// 	account_id := os.Getenv("META_WHATSAPP_BUSINESS_ID")
+// 	sender_id := os.Getenv("META_WHATSAPP_SENDER_PHONE_NUMBER")
 
-}
-func Respond() {}
+// }
+// func Respond() {}
--- demo/main.go
+++ demo/main.go
@@ -1,6 +1,11 @@
 package main
 
 import (
+	"bytes"
+	"fmt"
+	"io"
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
@@ -15,8 +20,44 @@ func main() {
 
 	server := gin.Default()
 
-	server.GET("/demo")
-	server.POST("/demo")
+	server.GET("/", func(c *gin.Context) {
+		c.String(http.StatusOK, "Sample Flow app")
+	})
 
-	server.Run()
+	server.GET("/demo", func(c *gin.Context) {
+
+		token := c.Query("hub.verify_token")
+		challenge := c.Query("hub.challenge")
+
+		if token == "sample_flow_app" {
+			c.String(http.StatusOK, challenge)
+		}
+
+	})
+
+	server.POST("/demo", func(c *gin.Context) {
+
+		data, err := io.ReadAll(c.Request.Body)
+
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Could not read body"})
+			return
+		}
+
+		io.NopCloser(bytes.NewBuffer(data))
+
+		data_string := string(data)
+
+		fmt.Println("Raw Body Received:", data_string)
+
+		c.JSON(http.StatusOK, gin.H{
+			"message":         "Raw body processed",
+			"raw_data_string": data_string,
+		})
+
+		fmt.Println()
+
+	})
+
+	server.Run(":9000")
 }
